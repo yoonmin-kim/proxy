c8239845dc576dc6e7142ac76c429572f233661d
yoonmin-kim <rladbsals23@gmail.com>
2021-11-23 21:37:47 +0900

    feat: 구체 클래스 기반 프록시 - 적용
    인터페이스 기반 프록시에 비해 몇 가지
    제약사항이 존재한다.
    
    - 부모 클래스의 생성자를 super()로 호출해야한다
    - final 이 붙은 클래스에 사용불가
    - final 이 붙은 메소드에 사용불가

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -7,13 +7,15 @@ import org.springframework.context.annotation.Import;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
 // @Import(AppV1Config.class)
 // @Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+// @Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
