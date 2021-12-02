89547fcf8493a47720828c5fa145563da5e67ffe
yoonmin-kim <rladbsals23@gmail.com>
2021-12-02 16:45:34 +0900

    feat: 예제코드1 - 어드바이저
    포인트컷: 어디에? 어드바이스를 적용할 것인지 정의한다
    어드바이스: 부가기능을 담당한다
    어드바이저: 포인트컷과 어드바이스를 함께 관리한다
    
    proxyFactory.addAdvice() 의 내부 로직을 살펴보면
    new DefaultPointcutAdvisor(Pointcut.TRUE, ${어드바이스}) 를 사용하고
    있는 것을 확인할 수 있다
    
    이번 예제에서는 proxyFactory.addAdvisors()를 사용하여 직접 어드바이저를
    설정해 보았다

---
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,26 @@
+package hello.proxy.advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+
+public class AdvisorTest {
+
+	@Test
+	void advisorTest1() {
+		ServiceInterface target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+		proxyFactory.addAdvisors(advisor);
+		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+
+		proxy.save();
+		proxy.find();
+
+	}
+}
