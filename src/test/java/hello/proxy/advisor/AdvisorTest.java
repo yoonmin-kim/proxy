d4ea72baac0b314c2dfc62b17d0574304fbe06e6
yoonmin-kim <rladbsals23@gmail.com>
2021-12-02 17:06:35 +0900

    feat: 예제코드2 - 직접 만든 포인트컷

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -1,6 +1,10 @@
 package hello.proxy.advisor;
 
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
@@ -8,7 +12,9 @@ import org.springframework.aop.support.DefaultPointcutAdvisor;
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AdvisorTest {
 
 	@Test
@@ -23,4 +29,53 @@ public class AdvisorTest {
 		proxy.find();
 
 	}
+
+	@Test
+	void advisorTest2() {
+		ServiceInterface target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointCut(), new TimeAdvice());
+		proxyFactory.addAdvisors(advisor);
+		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+
+		proxy.save();
+		proxy.find();
+
+	}
+
+	static class MyPointCut implements Pointcut {
+		@Override
+		public ClassFilter getClassFilter() {
+			return ClassFilter.TRUE;
+		}
+
+		@Override
+		public MethodMatcher getMethodMatcher() {
+			return new MyMethodMatcher();
+		}
+	}
+
+	static class MyMethodMatcher implements MethodMatcher {
+
+		public String matchName = "save";
+
+		@Override
+		public boolean matches(Method method, Class<?> targetClass) {
+			boolean result = matchName.equals(method.getName());
+			log.info("포인트컷 호출 method={} target={}", method.getName(), targetClass);
+			log.info("포인트컷 결과 result={}", result);
+			return false;
+		}
+
+		@Override
+		public boolean isRuntime() {
+			return false;
+		}
+
+		@Override
+		public boolean matches(Method method, Class<?> targetClass, Object... args) {
+			return false;
+		}
+	}
+
 }
