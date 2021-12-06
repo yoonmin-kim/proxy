fd3ec7a6010fe140de63c939e57fffce0170ee61
yoonmin-kim <rladbsals23@gmail.com>
2021-12-06 23:00:00 +0900

    feat: 스프링이 제공하는 포인트컷, 여러 어드바이저 적용
    NameMatchMethodPointcut 외에 여러 포인트컷을 스프링이 제공한다.
    
    어드바이저를 여러개 적용한다고 그 수만큼 프록시가 생성되는게 아니다
    스프링은 최적화를 통해 하나의 프록시에 여러 어드바이저를 적용할 수 있도록
    기능을 제공한다.

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -2,12 +2,16 @@ package hello.proxy.advisor;
 
 import java.lang.reflect.Method;
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.ClassFilter;
 import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
@@ -31,10 +35,27 @@ public class AdvisorTest {
 	}
 
 	@Test
+	@DisplayName("직접 만드는 포인트컷")
 	void advisorTest2() {
 		ServiceInterface target = new ServiceImpl();
 		ProxyFactory proxyFactory = new ProxyFactory(target);
 		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointCut(), new TimeAdvice());
+		proxyFactory.addAdvisors(advisor);
+		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
+
+		proxy.save();
+		proxy.find();
+
+	}
+
+	@Test
+	@DisplayName("스프링이 제공하는 포인트컷")
+	void advisorTest3() {
+		ServiceInterface target = new ServiceImpl();
+		ProxyFactory proxyFactory = new ProxyFactory(target);
+		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+		pointcut.setMappedName("save");
+		DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
 		proxyFactory.addAdvisors(advisor);
 		ServiceInterface proxy = (ServiceInterface)proxyFactory.getProxy();
 
