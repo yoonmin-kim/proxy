19af731b3330ce66e803895829dc2f38aa1c93ab
yoonmin-kim <rladbsals23@gmail.com>
2021-12-11 19:55:22 +0900

    feat: 스프링이 제공하는 빈 후처리기2
    NameMatchMethodPointcut를 사용하면 스프링에 등록된 모든 빈을 대상으로
    메소드의 이름만 판단해서 프록시를 등록하게 되어서, 보다 정밀한 대상판단을 위한
    포인트 컷이 필요했다.
    
    AspectJExpressionPointcut를 사용하면 원하는 반환타입, 패키지, 메소드, 파라미터 등
    보다 정밀하게 포인트컷의 대상을 지정할 수 있다.

--- src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -1,6 +1,7 @@
 package hello.proxy.config.v5_autoproxy;
 
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +17,26 @@ import hello.proxy.trace.logtrace.LogTrace;
 @Import(value = {AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-	@Bean
+	// @Bean
 	public Advisor advisor1(LogTrace logTrace) {
 		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
 		pointcut.setMappedNames("request*", "order*", "save*");
+		LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+		return new DefaultPointcutAdvisor(pointcut, advice);
+	}
+
+	// @Bean
+	public Advisor advisor2(LogTrace logTrace) {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..))");
+		LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+		return new DefaultPointcutAdvisor(pointcut, advice);
+	}
+
+	@Bean
+	public Advisor advisor3(LogTrace logTrace) {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
 		LogTraceAdvice advice = new LogTraceAdvice(logTrace);
 		return new DefaultPointcutAdvisor(pointcut, advice);
 	}
