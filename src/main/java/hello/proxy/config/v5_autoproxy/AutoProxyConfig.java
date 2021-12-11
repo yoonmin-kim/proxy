8c1e0e7f911d864522394e3d91b35f8f1b772a45
yoonmin-kim <rladbsals23@gmail.com>
2021-12-11 19:26:10 +0900

    feat: 스프링이 제공하는 빈 후처리기1
    build.gradle에 implementation 'org.springframework.boot:spring-boot-starter-aop'
    를 추가하면 스프링 부트가 AnnotationAwareAspectJAutoProxyCreator 라는 빈 후처리기로
    어드바이저들을 찾아서 프록시를 적용할 빈들을 모두 찾아낸다
    
    포인트컷의 경우 프록시로 등록 될 대상들을 판단할때 사용되며
    프록시빈 내부에서 어드바이저가 적용 될 메소드를 판단할때도 사용된다.

---
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,26 @@
+package hello.proxy.config.v5_autoproxy;
+
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+
+@Configuration
+@Import(value = {AppV1Config.class, AppV2Config.class})
+public class AutoProxyConfig {
+
+	@Bean
+	public Advisor advisor1(LogTrace logTrace) {
+		NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+		pointcut.setMappedNames("request*", "order*", "save*");
+		LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+		return new DefaultPointcutAdvisor(pointcut, advice);
+	}
+}
